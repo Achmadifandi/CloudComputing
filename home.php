01a758fceb65f8049a6f2b6a268798f7167d39a0
Achmadifandi <50076866+Achmadifandi@users.noreply.github.com>
2019-04-28 20:13:52 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,75 @@
+<!doctype html>
+ <html>
+ <head>
+ <title>Data Diriku</title>
+ <link rel="stylesheet" href="css biodata.css"/>
+ </head>
+ <body>
+ <form action="#" style="width: 1000px"class="posisi";>
+ <fieldset class="h"/>
+ <table style="width: 980px;">
+ <tr>
+ <td rowspan="15" width="250px">
+ <img src="joker.jpeg" width="288" height="440"/>
+ </td>
+ </tr>
+ <tr>
+ <td><b>Nama Lengkap</b></td>
+ <td>:</td>
+ <td>Achmad Ifandi Yazid</td>
+ </tr>
+ <tr>
+ <td><b>Nama Panggilan</b></td>
+ <td>:</td>
+ <td>Joker</td>
+ </tr>
+ <tr>
+ <td><b>Tempat, Tanggal Lahir</b></td>
+ <td>:</td>
+ <td>Sidoarjo, 24 Juli 1997</td>
+ </tr>
+ <tr>
+ <td><b>Umur</b></td>
+ <td>:</td>
+ <td>21 Tahun</td>
+ </tr>
+ <tr>
+ <td><b>Jenis Kelamin</b></td>
+ <td>:</td>
+ <td>Laki - Laki</td>
+ </tr>
+ <tr>
+ <td><b>Gol. Darah</b></td>
+ <td>:</td>
+ <td>B</td>
+ </tr>
+ <tr>
+ <td height="27"><b>Agama</b></td>
+ <td>:</td>
+ <td>Islam</td>
+ </tr>
+ <tr>
+ <td><b>Alamat</b></td>
+ <td>:</td>
+ <td>Taman Rt 01 Rw 01 Sepanjang</td>
+ </tr>
+ <tr>
+ <td><b>Angkatan</b></td>
+ <td>:</td>
+ <td>2015</td>
+ </tr>
+ <tr>
+ <td><b>Jurusan</b></td>
+ <td>:</td>
+ <td>Teknik Informatika</td>
+ </tr>
+ <tr>
+ <td>&nbsp;</td>
+ <td>&nbsp;</td>
+ <td colspan="1" align="left">&nbsp;</td>
+ </tr>
+ </table>
+ </fieldset>
+ </form>
+ </body>
+ </html>
